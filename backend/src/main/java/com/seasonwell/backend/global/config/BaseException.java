f25b9b6b89fedbf68feafe1f48a91c05b8f7637b
hyun-jung-joo <wnguswjd0418@naver.com>
2023-11-13 18:25:48 +0900

    [init] project init

---
+++ backend/src/main/java/com/seasonwell/backend/global/config/BaseException.java
@@ -0,0 +1,10 @@
+package com.seasonwell.backend.global.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BaseException extends Exception {
+    private ResponseStatus status;
+}
