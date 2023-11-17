b3a734849d7f36f5789f1e8ecdd0b20f1f02ef70
LEEJH1029 <uamybest4@naver.com>
2023-11-17 22:52:14 +0900

    [Fix] user 반환 타입 수정

--- backend/src/main/java/com/seasonwell/backend/user/controller/MyPageController.java
+++ backend/src/main/java/com/seasonwell/backend/user/controller/MyPageController.java
@@ -1,14 +1,14 @@
 package com.seasonwell.backend.user.controller;
 
 import com.seasonwell.backend.global.config.BaseException;
-import com.seasonwell.backend.global.config.BaseResponse;
-import com.seasonwell.backend.global.config.ResponseStatus;
 import com.seasonwell.backend.user.dto.MyPageResponse;
 import com.seasonwell.backend.user.dto.UpdateUserRequest;
 import com.seasonwell.backend.user.entity.User;
 import com.seasonwell.backend.user.service.MyPageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
@@ -21,29 +21,29 @@ public class MyPageController {
     private final MyPageService mypageService;
 
     @GetMapping("/userinfo")
-    public BaseResponse<MyPageResponse> memberInfo(HttpSession session) {
+    public ResponseEntity<MyPageResponse> memberInfo(HttpSession session) {
         try {
             String userId = (String) session.getAttribute("userId");
             User currentUser = mypageService.loadMemberByUsername(userId);
             log.info(userId);
 
-            return new BaseResponse<>(new MyPageResponse(currentUser));
+            return new ResponseEntity<>(new MyPageResponse(currentUser), HttpStatus.OK);
         } catch (BaseException e) {
-            return new BaseResponse<>(ResponseStatus.INVALID_AUTH);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
     @PutMapping("/userinfo/update")
-    public BaseResponse<String> updateInfo(
+    public ResponseEntity<String> updateInfo(
             @RequestBody UpdateUserRequest updateUserRequest,
             HttpSession session
     ) {
         try {
             mypageService.updateUser(updateUserRequest, session);
             String result = "회원정보 변경 완료";
-            return new BaseResponse<>(result);
+            return new ResponseEntity<>(result, HttpStatus.OK);
         } catch (BaseException e) {
-            return new BaseResponse<>(ResponseStatus.INVALID_AUTH);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 }
