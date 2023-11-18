669bad32c7b944a00c2261387e0a9b81aa941049
hyun-jung-joo <wnguswjd0418@naver.com>
2023-11-18 23:01:52 +0900

    [fix] 카카오 수정

--- backend/src/main/java/com/seasonwell/backend/auth/controller/AuthController.java
+++ backend/src/main/java/com/seasonwell/backend/auth/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,11 +25,10 @@ public class AuthController {
     }
 
     @GetMapping("/kakao")
-    public ResponseEntity<LoginResponseDto> kakaoLogin(HttpServletRequest request) {
-        String code = request.getParameter("code");
+    public ResponseEntity<LoginResponseDto> kakaoLogin(@RequestParam("code") String code) {
         String kakaoAccessToken = authService.getKakaoAccessToken(code);
-        log.debug("code={}", kakaoAccessToken);
         return authService.kakaoLogin(kakaoAccessToken);
     }
+
 
 }
--- backend/src/main/java/com/seasonwell/backend/auth/service/AuthService.java
+++ backend/src/main/java/com/seasonwell/backend/auth/service/AuthService.java
@@ -11,22 +11,22 @@ import com.seasonwell.backend.user.entity.User;
 import com.seasonwell.backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.util.MultiValueMap;
-import javax.transaction.Transactional;
 
 @RequiredArgsConstructor
-@Transactional
 @Service
 @Slf4j
 public class AuthService {
+
     @Value("${api.kakao-rest-api-key}")
     private String apiKey;
 
@@ -34,113 +34,106 @@ public class AuthService {
 
     @Transactional
     public String getKakaoAccessToken(String code) {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        // Http Response Body 객체 생성
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "authorization_code"); //고정값
-        params.add("client_id", apiKey);
-        params.add("redirect_uri", "https://seasonwell.vercel.app/auth/login/kakao"); //등록한 redirect uri
-        params.add("code", code);
-
-        // 헤더와 바디 합치기 위해 Http Entity 객체 생성
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
-        log.debug("url = {}", kakaoTokenRequest);
-
-        // 카카오로부터 Access token 받아오기
-        RestTemplate rt = new RestTemplate();
-        ResponseEntity<String> accessTokenResponse = rt.exchange(
-                "https://kauth.kakao.com/oauth/token",
-                HttpMethod.POST,
-                kakaoTokenRequest,
-                String.class
-        );
-
-        // JSON Parsing (-> KakaoTokenDto)
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        KakaoTokenDto kakaoTokenDto = null;
         try {
-            kakaoTokenDto = objectMapper.readValue(accessTokenResponse.getBody(), KakaoTokenDto.class);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-        return kakaoTokenDto.getAccessToken(); // accesstoken 반환
+            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+            params.add("grant_type", "authorization_code");
+            params.add("client_id", apiKey);
+            params.add("redirect_uri", "https://seasonwell.vercel.app/auth/login/kakao");
+            params.add("code", code);
+
+            HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+            RestTemplate rt = new RestTemplate();
+            ResponseEntity<String> accessTokenResponse = rt.exchange(
+                    "https://kauth.kakao.com/oauth/token",
+                    HttpMethod.POST,
+                    kakaoTokenRequest,
+                    String.class
+            );
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            KakaoTokenDto kakaoTokenDto = objectMapper.readValue(accessTokenResponse.getBody(), KakaoTokenDto.class);
+
+            return kakaoTokenDto.getAccessToken();
+        } catch (Exception e) {
+            log.error("Error in getKakaoAccessToken", e);
+            throw new RuntimeException("Failed to get Kakao access token", e);
+        }
     }
 
     public ResponseEntity<LoginResponseDto> kakaoLogin(String kakaoAccessToken) {
-        User account = getKakaoInfo(kakaoAccessToken);
-
-        LoginResponseDto loginResponseDto = new LoginResponseDto();
-        loginResponseDto.setLoginSuccess(true);
-        loginResponseDto.setUser(account);
-
-        User existOwner = userRepository.findByUserId(account.getUserId()).orElse(null);
         try {
-            if (existOwner == null) {
-                System.out.println("처음 로그인 하는 회원입니다.");
-                userRepository.save(account);
-            }
-            loginResponseDto.setLoginSuccess(true);
+            User account = getKakaoInfo(kakaoAccessToken);
 
-            return ResponseEntity.ok().body(loginResponseDto);
+            LoginResponseDto loginResponseDto = new LoginResponseDto();
+            loginResponseDto.setLoginSuccess(true);
+            loginResponseDto.setUser(account);
+
+            User existOwner = userRepository.findByUserId(account.getUserId()).orElse(null);
+
+            try {
+                if (existOwner == null) {
+                    System.out.println("처음 로그인 하는 회원입니다.");
+                    userRepository.save(account);
+                }
+                loginResponseDto.setLoginSuccess(true);
+
+                return ResponseEntity.ok().body(loginResponseDto);
+            } catch (Exception e) {
+                loginResponseDto.setLoginSuccess(false);
+                return ResponseEntity.badRequest().body(loginResponseDto);
+            }
         } catch (Exception e) {
-            loginResponseDto.setLoginSuccess(false);
-            return ResponseEntity.badRequest().body(loginResponseDto);
+            log.error("Error in kakaoLogin", e);
+            throw new RuntimeException("Failed to perform Kakao login", e);
         }
     }
 
     public User getKakaoInfo(String kakaoAccessToken) {
-        RestTemplate rt = new RestTemplate();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + kakaoAccessToken);
-        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        HttpEntity<MultiValueMap<String, String>> accountInfoRequest = new HttpEntity<>(headers);
-
-        // POST 방식으로 API 서버에 요청 후 response 받아옴
-        ResponseEntity<String> accountInfoResponse = rt.exchange(
-                "https://kapi.kakao.com/v2/user/me",
-                HttpMethod.POST,
-                accountInfoRequest,
-                String.class
-        );
-
-        // JSON Parsing (-> kakaoAccountDto)
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-        KakaoAccountDto kakaoAccountDto = null;
         try {
-            kakaoAccountDto = objectMapper.readValue(accountInfoResponse.getBody(), KakaoAccountDto.class);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+            RestTemplate rt = new RestTemplate();
 
-        // 회원가입 처리하기
-        String kakaoId = kakaoAccountDto.getUser_id();
-        User existOwner = userRepository.findByUserId(kakaoId).orElse(null);
-        // 처음 로그인이 아닌 경우
-        if (existOwner != null) {
-            return User.builder()
-                    .userId(kakaoAccountDto.getUser_id())
-                    .user_pw(kakaoAccountDto.getUser_pw())
-                    .build();
-        }
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", "Bearer " + kakaoAccessToken);
+            headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-        // 처음 로그인 하는 경우
-        else {
-            return User.builder()
-                    .userId(kakaoAccountDto.getUser_id())
-                    .user_pw(kakaoAccountDto.getUser_pw())
-                    .build();
+            HttpEntity<MultiValueMap<String, String>> accountInfoRequest = new HttpEntity<>(headers);
+
+            ResponseEntity<String> accountInfoResponse = rt.exchange(
+                    "https://kapi.kakao.com/v2/user/me",
+                    HttpMethod.POST,
+                    accountInfoRequest,
+                    String.class
+            );
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+            KakaoAccountDto kakaoAccountDto = objectMapper.readValue(accountInfoResponse.getBody(), KakaoAccountDto.class);
+
+            String kakaoId = kakaoAccountDto.getUser_id();
+            User existOwner = userRepository.findByUserId(kakaoId).orElse(null);
+
+            if (existOwner != null) {
+                return User.builder()
+                        .userId(kakaoAccountDto.getUser_id())
+                        .user_pw(kakaoAccountDto.getUser_pw())
+                        .build();
+            } else {
+                return User.builder()
+                        .userId(kakaoAccountDto.getUser_id())
+                        .user_pw(kakaoAccountDto.getUser_pw())
+                        .build();
+            }
+        } catch (Exception e) {
+            log.error("Error in getKakaoInfo", e);
+            throw new RuntimeException("Failed to get Kakao user information", e);
         }
     }
-
 }
