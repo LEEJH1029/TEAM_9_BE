f64d622c68f4446cc17633571110f9e4660875e5
LEEJH1029 <uamybest4@naver.com>
2023-11-18 19:24:54 +0900

    [Add] 영양제 이름 검색 추가

--- backend/src/main/java/com/seasonwell/backend/nutrients/repository/NutrientsRepository.java
+++ backend/src/main/java/com/seasonwell/backend/nutrients/repository/NutrientsRepository.java
@@ -1,5 +1,6 @@
 package com.seasonwell.backend.nutrients.repository;
 
+import com.seasonwell.backend.board.entity.Board;
 import com.seasonwell.backend.nutrients.entity.Nutrients;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +15,11 @@ public interface NutrientsRepository extends JpaRepository<Nutrients, Long> {
     List<Nutrients> findAll();
 
     Optional<Nutrients> findById(Long id);
+
+    @Query("SELECT m FROM Nutrients m WHERE lower(m.nutrients_name) LIKE lower(concat('%', :nutrients_name, '%'))")
+    List<Nutrients> findByNutrients_name(
+            @Param("nutrients_name") String nutrients_name
+    );
 
     @Query("SELECT n FROM Nutrients n INNER JOIN n.diseaseNutrients dn INNER JOIN dn.disease d WHERE d.disease_code = :diseaseCode")
     List<Nutrients> findAllByDiseaseCode(@Param("diseaseCode") String diseaseCode);
